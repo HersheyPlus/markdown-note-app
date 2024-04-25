43b16870a55117a3e6a00e255c44cd72893d6b07
Melbromss <melbromss@gmail.com>
2024-04-25 21:32:15 +0700

    add edition

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,13 @@
-import { Sidebar, RootLayout, Content, DraggableTopbar, ActionButtonsRow, NotePreviewList } from '@/components'
-
+import {
+  Sidebar,
+  RootLayout,
+  Content,
+  DraggableTopbar,
+  ActionButtonsRow,
+  NotePreviewList
+} from '@/components'
+import MarkdonwEditor from './components/MarkdonwEditor'
+import FloatingNoteTitle from './components/FloatingNoteTitle'
 
 function App(): JSX.Element {
   return (
@@ -10,7 +18,10 @@ function App(): JSX.Element {
           <ActionButtonsRow className="flex justify-between mt-1" />
           <NotePreviewList className="mt-4 space-y-1" />
         </Sidebar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">Contene</Content>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">
+          <FloatingNoteTitle className="pt-2" />
+          <MarkdonwEditor />
+        </Content>
       </RootLayout>
     </>
   )
---
+++ src/renderer/src/components/FloatingNoteTitle.tsx
@@ -0,0 +1,13 @@
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+const FloatingNoteTitle = ({ className, ...props }: ComponentProps<'div'>) => {
+  const title = 'Note title'
+  return (
+    <div className={twMerge('flex justify-center', className)} {...props}>
+      <span className="text-gray-400">{title}</span>
+    </div>
+  )
+}
+
+export default FloatingNoteTitle
